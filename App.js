e31540c70533d1895090bff102ae8d847d28bb14
diviam <diviamsingh684@gmail.com>
2023-07-21 14:20:30 +0530

    contact Us Screen created

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { SafeAreaView } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 import GetStartedScreen from './src/components/Screens/GetStartedScreen';
 import HomePageScreen from './src/components/Screens/HomePageScreen';
+import ContactUs from './src/components/Screens/ContactUsScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 export default function App() {
@@ -13,8 +14,9 @@ export default function App() {
       <Stack.Navigator initialRouteName="GetStarted">
         <Stack.Screen name="GetStarted" component={GetStartedScreen} options={{ headerShown: false }} />
         <Stack.Screen name="HomePage" component={HomePageScreen} />
+        <Stack.Screen name="ContactUs" component={ContactUs} />
       </Stack.Navigator>
-    </NavigationContainer>
+    </NavigationContainer>  
   );
 }
 
